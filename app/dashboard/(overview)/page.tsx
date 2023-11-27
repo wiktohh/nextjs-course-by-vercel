bacf00bc71a4e2027eb72a44ea8b9339e654fc59
wiktohh <wiktor.rzeznicki01@gmail.com>
2023-11-27 17:43:44 +0100

    adding metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -9,6 +9,11 @@ import {
   CardsSkeleton,
 } from '@/app/ui/skeletons';
 import { fetchCardData } from '@/app/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page() {
   const {
